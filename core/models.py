afb6597092f4702e916ac47b8dc640ae53df1120
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-03 20:23:53 +0600

    model change and duration count

--- core/models.py
+++ core/models.py
@@ -1,10 +1,16 @@
+from datetime import datetime, date
 from django.db import models
 
-# Create your models here.
+
 class Trip(models.Model):
     trip_from = models.CharField(max_length=200)
-    to = models.CharField(max_length=200)
-    at = models.DateTimeField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
+    def duration(self):
+        d = datetime.combine(date.min, self.end_time) - \
+            datetime.combine(date.min, self.start_time)
+        return f'{d}'
+    
     def __str__(self):
-        return f'{self.trip_from} to {self.to} at {self.at}'
+        return f'{self.trip_from} tour for {self.duration()}'
